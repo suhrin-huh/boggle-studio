2814d5e85449e97241a13b147467cee29b358b00
황민채 <trick0846@naver.com>
2026-04-28 22:41:35 +0900

    fix: 홈페이지 UI 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -7,12 +7,10 @@ export default function HomePage() {
   const router = useRouter();
 
   return (
-    <div className="mx-auto my-50 h-full max-h-150 min-h-125 w-full max-w-100 min-w-80">
-      <div className="flex flex-col items-center justify-center gap-2 p-4">
-        <h1 className="font-pixel">BOGGLE BOGGLE STUDIO</h1>
-        <p>집에서 편리하게 촬영하는 인생네컷</p>
-        <PixelButton onClick={() => router.push('/frame')}>START</PixelButton>
-      </div>
+    <div className="p-sm flex flex-1 flex-col items-center justify-center gap-2">
+      <p className="font-pixel text-primary text-body-sm md:text-body-lg">BOGGLE BOGGLE STUDIO</p>
+      <p>집에서 편리하게 촬영하는 인생네컷</p>
+      <PixelButton onClick={() => router.push('/frame')}>START</PixelButton>
     </div>
   );
 }
