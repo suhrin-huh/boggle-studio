3e56ab07b9c70540c913ece8d59dda285f157e8f
황민채 <trick0846@naver.com>
2026-04-28 22:41:20 +0900

    fix: global layout 크기 오류 수정

--- src/app/_components/AppShell.tsx
+++ src/app/_components/AppShell.tsx
@@ -1,18 +1,12 @@
-import PixelWindow from '@/components/common/PixelWindow';
-
 interface AppShellProps {
   children: React.ReactNode;
 }
 
 export default function AppShell({ children }: AppShellProps) {
   return (
-    <div className="flex h-screen min-h-[600px] w-dvw min-w-[360px] items-center justify-center bg-[#64BEF0]">
-      <main className="relative flex max-w-[480px] flex-1 shrink-0 flex-col rounded-lg bg-white shadow-2xl">
-        <PixelWindow title="BB STUDIO">
-          <div className="mx-auto my-50 h-full max-h-150 min-h-125 w-full max-w-100 min-w-80">
-            {children}
-          </div>
-        </PixelWindow>
+    <div className="bg-primary p-md flex h-dvh min-h-150 min-w-80 justify-center">
+      <main className="relative flex w-full max-w-120 flex-col overflow-hidden rounded-lg bg-white shadow-2xl">
+        {children}
       </main>
     </div>
   );
