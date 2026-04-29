ebe927433803ecfa7f287cc3a82741cee4ed17a0
황민채 <trick0846@naver.com>
2026-04-30 01:24:50 +0900

    feat: Header 생성 및 적용

--- src/app/_components/AppShell.tsx
+++ src/app/_components/AppShell.tsx
@@ -1,3 +1,5 @@
+import Header from '@/components/common/Header';
+
 interface AppShellProps {
   children: React.ReactNode;
 }
@@ -6,6 +8,7 @@ export default function AppShell({ children }: AppShellProps) {
   return (
     <div className="bg-primary p-md flex h-dvh min-h-150 min-w-80 justify-center">
       <main className="relative flex w-full max-w-120 flex-col overflow-hidden rounded-lg bg-white shadow-2xl">
+        <Header />
         {children}
       </main>
     </div>
