4ad288ebf99206dc7d898dd801068959c0401b9d
황민채 <trick0846@naver.com>
2026-04-29 20:07:29 +0900

    feat: CameraCapture 생성

---
+++ src/app/capture/_components/CameraCapture.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import Webcam from 'react-webcam';
+import useCamera from '@/hooks/useCamera';
+import { MAX_PHOTO_COUNT } from '@/constants/themes';
+
+const VIDEO_CONSTRAINTS = {
+  width: 800,
+  height: 600,
+  facingMode: 'user',
+};
+
+interface CameraCaptureProps {
+  capturedCuts: string[];
+  onCapture: (data: string) => void;
+}
+
+/**
+ * 카메라 촬영 전용 컴포넌트
+ * @param capturedCuts : 촬영된 사진 데이터
+ * @param onCapture : 촬영하기 버튼 클릭 시 동작할 이벤트 함수
+ */
+export default function CameraCapture({ capturedCuts, onCapture }: CameraCaptureProps) {
+  const { webcamRef, capture } = useCamera();
+
+  const handleCapture = () => {
+    if (capturedCuts.length >= MAX_PHOTO_COUNT) return;
+    const screenshot = capture();
+    if (screenshot) onCapture(screenshot);
+  };
+
+  return (
+    <div>
+      <Webcam
+        ref={webcamRef}
+        audio={false}
+        screenshotFormat="image/png"
+        videoConstraints={VIDEO_CONSTRAINTS}
+        style={{ width: 400, height: 300 }}
+      />
+      <br />
+      <button onClick={handleCapture} disabled={capturedCuts.length >= MAX_PHOTO_COUNT}>
+        촬영하기
+      </button>
+      {capturedCuts.map((src, i) => (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img key={i} src={src} alt={`촬영 ${i + 1}`} style={{ width: 100, height: 75 }} />
+      ))}
+    </div>
+  );
+}
