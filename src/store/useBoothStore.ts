50de4fcbb6030876ec27eae7b86ff261226b7080
황민채 <trick0846@naver.com>
2026-05-01 02:58:52 +0900

    refactor: store 타입 선언 위치 변경

--- src/store/useBoothStore.ts
+++ src/store/useBoothStore.ts
@@ -1,5 +1,22 @@
 import { create } from 'zustand';
-import { BoothStore } from '@/types/booth';
+import { ThemeOption } from '@/types/booth';
+import { FrameId } from '@/constants/booth';
+
+export interface BoothState {
+  selectedTheme: ThemeOption | null;
+  frameId: FrameId | null;
+  capturedCuts: string[];
+  finalImageName: string;
+}
+
+export interface BoothActions {
+  setTheme: (theme: ThemeOption) => void;
+  setFrameId: (id: FrameId) => void;
+  addCapture: (imageData: string) => void;
+  resetBooth: () => void;
+}
+
+export type BoothStore = BoothState & BoothActions;
 
 /**
  * 인생 네컷 생성에 필요한 전역 변수 관리
@@ -12,9 +29,12 @@ import { BoothStore } from '@/types/booth';
  */
 export const useBoothStore = create<BoothStore>((set) => ({
   selectedTheme: null,
+  frameId: null,
   capturedCuts: [],
   finalImageName: '',
   setTheme: (theme) => set({ selectedTheme: theme }),
+  setFrameId: (id) => set({ frameId: id }),
   addCapture: (imageData) => set((state) => ({ capturedCuts: [...state.capturedCuts, imageData] })),
-  resetBooth: () => set({ selectedTheme: null, capturedCuts: [], finalImageName: '' }),
+  resetBooth: () =>
+    set({ selectedTheme: null, frameId: null, capturedCuts: [], finalImageName: '' }),
 }));
--- src/types/booth.ts
+++ src/types/booth.ts
@@ -4,20 +4,6 @@ export interface ThemeOption {
   value: string;
 }
 
-export interface BoothState {
-  selectedTheme: ThemeOption | null;
-  capturedCuts: string[];
-  finalImageName: string;
-}
-
-export interface BoothActions {
-  setTheme: (theme: ThemeOption) => void;
-  addCapture: (imageData: string) => void;
-  resetBooth: () => void;
-}
-
-export type BoothStore = BoothState & BoothActions;
-
 export type CaptureMode = 'camera' | 'upload';
 
 export interface FrameConfig {
