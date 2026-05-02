5029727c1893ef231ce4e7204768252646b7f4f5
황민채 <trick0846@naver.com>
2026-05-02 19:51:08 +0900

    feat: frame config 업데이트 및 프레임 요소 추가

--- src/constants/booth.ts
+++ src/constants/booth.ts
@@ -24,37 +24,82 @@ export type CaptureMode = (typeof CAPTURE_MODE)[keyof typeof CAPTURE_MODE]['id']
 /**
  * 현재 프레임 미완성으로 모두 동일한 sample image를 보여준다.
  * => next/image + Config 데이터의 width, height 활용하여 CLS 방지
+ * id, label, width, height, requiredPhotoCount, sampleImageUrl, frameImageUrl, overlayImageUrl, slots
  */
 export const FRAMES = {
   basic_black: {
     id: 'basic_black',
     label: '기본 블랙',
-    width: 1000,
-    height: 3000,
+    width: 600,
+    height: 1800,
     requiredPhotoCount: 4,
-    backgroundColor: '#000000',
-    frameImageUrl: null,
-    sampleImageUrl: '/images/frames/frame.png',
+    frameImageUrl: '/images/frames/basic-black.png',
+    sampleImageUrl: '/images/frames/basic-black.png',
+    overlayImageUrl: null,
+    slots: [
+      { x: 30, y: 110, width: 540, height: 405 }, // 첫 번째 사진
+      { x: 30, y: 525, width: 540, height: 405 }, // 두 번째 사진
+      { x: 30, y: 940, width: 540, height: 405 }, // 세 번째 사진
+      { x: 30, y: 1355, width: 540, height: 405 }, // 네 번째 사진
+    ],
   },
   basic_white: {
     id: 'basic_white',
     label: '기본 화이트',
-    width: 1000,
-    height: 3000,
+    width: 600,
+    height: 1800,
     requiredPhotoCount: 4,
-    backgroundColor: '#FFFFFF',
-    frameImageUrl: null,
-    sampleImageUrl: '/images/frames/frame.png',
+    frameImageUrl: '/images/frames/basic-white.png',
+    sampleImageUrl: '/images/frames/basic-white.png',
+    overlayImageUrl: null,
+    slots: [
+      { x: 30, y: 110, width: 540, height: 405 }, // 첫 번째 사진
+      { x: 30, y: 525, width: 540, height: 405 }, // 두 번째 사진
+      { x: 30, y: 940, width: 540, height: 405 }, // 세 번째 사진
+      { x: 30, y: 1355, width: 540, height: 405 }, // 네 번째 사진
+    ],
   },
-  // fish_theme: {
-  //   id: 'fish_theme',
-  //   label: '물고기 테마',
-  //   width: 2000,
-  //   height: 3000,
+  dark_denim: {
+    id: 'dark_denim',
+    label: '다크 데님',
+    width: 600,
+    height: 1800,
+    requiredPhotoCount: 4,
+    frameImageUrl: '/images/frames/dark-denim.png',
+    sampleImageUrl: '/images/frames/dark-denim.png',
+    overlayImageUrl: '/images/overlays/blue-silver-1.png',
+    slots: [
+      { x: 30, y: 110, width: 540, height: 405 }, // 첫 번째 사진
+      { x: 30, y: 525, width: 540, height: 405 }, // 두 번째 사진
+      { x: 30, y: 940, width: 540, height: 405 }, // 세 번째 사진
+      { x: 30, y: 1355, width: 540, height: 405 }, // 네 번째 사진
+    ],
+  },
+  light_denim: {
+    id: 'light_denim',
+    label: '라이트 데님',
+    width: 600,
+    height: 1800,
+    requiredPhotoCount: 4,
+    frameImageUrl: '/images/frames/light-denim.png',
+    sampleImageUrl: '/images/frames/light-denim.png',
+    overlayImageUrl: '/images/overlays/blue-silver-1.png',
+    slots: [
+      { x: 30, y: 110, width: 540, height: 405 }, // 첫 번째 사진
+      { x: 30, y: 525, width: 540, height: 405 }, // 두 번째 사진
+      { x: 30, y: 940, width: 540, height: 405 }, // 세 번째 사진
+      { x: 30, y: 1355, width: 540, height: 405 }, // 네 번째 사진
+    ],
+  },
+  // basic_white: {
+  //   id: 'basic_white',
+  //   label: '기본 화이트',
+  //   width: 600,
+  //   height: 1800,
   //   requiredPhotoCount: 4,
-  //   backgroundColor: null,
-  //   frameImageUrl: '/images/frames/fish_overlay.png',
-  //   sampleImageUrl: '/images/frames/frame.png', // 미리보기용 샘플 이미지
+  //   backgroundColor: '#FFFFFF',
+  //   frameImageUrl: null,
+  //   sampleImageUrl: '/images/frames/frame.png',
   // },
 } satisfies Record<string, FrameConfig>;
 
