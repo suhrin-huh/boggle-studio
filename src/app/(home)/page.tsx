ccb28796b4fe8df93b3cbb9f801870f660095ba8
황민채 <trick0846@naver.com>
2026-05-05 01:56:08 +0900

    feat: 홈페이지 UI 디자인 변경

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,42 @@
 'use client';
 
-import PixelButton from '@/components/common/PixelButton';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import serviceDemo from '@/assets/image/service-demo.gif';
+import introText from '@/assets/image/inrto-text.png';
+import PhotoFrame from './_components/PhotoFrame';
+import NeumorphicButton from '@/components/common/NeumorphicButton';
 
 export default function HomePage() {
   const router = useRouter();
 
   return (
-    <div className="p-sm flex flex-1 flex-col items-center justify-center gap-2">
-      <p className="font-pixel text-primary text-body-sm md:text-body-lg">BOGGLE BOGGLE STUDIO</p>
-      <p>집에서 편리하게 촬영하는 인생네컷</p>
-      <PixelButton onClick={() => router.push('/frame')}>START</PixelButton>
-    </div>
+    <main className="p-md flex flex-1 flex-col items-center justify-center gap-4 md:gap-6 lg:gap-8">
+      {/* 텍스트 컨텐츠 */}
+      <div className="font-unbounded flex flex-col gap-2 text-center md:gap-3">
+        <div className="relative h-35 w-80">
+          <Image src={introText} alt="서비스 소개글" fill loading="eager" className="rounded-sm" />
+        </div>
+        <p className="text-muted-dark text-[10px] md:text-[12px] lg:text-[14px]">
+          capture the unexpected.
+        </p>
+      </div>
+      <PhotoFrame className="aspect-3/4 w-60 md:w-70 lg:w-90">
+        <Image
+          src={serviceDemo}
+          alt="서비스 소개 GIF"
+          fill
+          loading="eager"
+          className="rounded-sm"
+          unoptimized
+        />
+      </PhotoFrame>
+      <p className="text-muted-dark font-unbounded text-[9px] md:text-[11px] lg:text-[13px]">
+        PLAYFUL · PHOTO · PIECES
+      </p>
+      <NeumorphicButton className="z-10" onClick={() => router.push('/frame')}>
+        START
+      </NeumorphicButton>
+    </main>
   );
 }
