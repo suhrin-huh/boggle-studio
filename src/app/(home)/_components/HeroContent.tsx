d256bdf4c171f570d50f492ad5af9e0a4ea58812
황민채 <trick0846@naver.com>
2026-05-14 19:40:42 +0900

    refactor: HeroContent 스타일 위치 수정

--- src/app/(home)/_components/HeroContent.tsx
+++ src/app/(home)/_components/HeroContent.tsx
@@ -3,16 +3,9 @@ import introText from '@/assets/image/intro-text.png';
 
 export default function HeroContent() {
   return (
-    <div className="font-unbounded gap-sm md:gap-md flex flex-col text-center">
-      <Image
-        src={introText}
-        alt="서비스 소개글"
-        width={240}
-        height={60}
-        className="rounded-sm"
-        priority
-      />
-      <p className="text-muted-dark text-caption">capture the unexpected.</p>
+    <div className="gap-sm flex flex-col text-center">
+      <Image src={introText} alt="서비스 소개글" width={240} height={60} priority />
+      <p className="text-muted-dark text-caption font-unbounded">capture the unexpected.</p>
     </div>
   );
 }
