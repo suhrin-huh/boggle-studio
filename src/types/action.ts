3343cad707112f2a632350f7edd99860900b388e
황민채 <trick0846@naver.com>
2026-05-21 17:54:07 +0900

    fix: ActionState 제네릭 기본값 설정

--- src/types/action.ts
+++ src/types/action.ts
@@ -3,7 +3,7 @@
  * TODO: type 위치를 좀 더 고민해봐도 좋을 것 같다.
  */
 
-export type ActionState<T> = {
+export type ActionState<T = void> = {
   success: boolean;
   message: string;
   error?: Record<string, string[]>;
