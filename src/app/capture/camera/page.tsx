5aa6ddd96a9f0832f6c93edc96f157656043bb99
황민채 <trick0846@naver.com>
2026-06-01 18:30:43 +0900

    feat: 촬영 페이지 PageTitle 제거

--- src/app/capture/camera/page.tsx
+++ src/app/capture/camera/page.tsx
@@ -1,12 +1,10 @@
 // components
 import PageContainer from '@/components/common/PageContainer';
-import PageTitle from '@/components/common/PageTitle';
 import CameraBooth from './_components/CameraBooth';
 
 export default function CameraPage() {
   return (
     <PageContainer className="justify-stretch">
-      <PageTitle title="Camera" />
       <CameraBooth />
     </PageContainer>
   );
